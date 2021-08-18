2e64e0c11ce0d18fb8df7fad1b10849f89d3b9ee
Shipan <liushipan1998@gmail.com>
2021-08-18 08:51:38 +0200

    tensor model works but wrong output

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 // import App from './src/test/App';
+// import App from './src/test/TMExampleProject/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/test/TeachMachineTest.js
+++ src/test/TeachMachineTest.js
@@ -7,6 +7,10 @@ decodeJpeg 函数可以代替 imageToTensor（）
 假如使用第三方库的话，比如： @tensorflow-models/mobilenet
 可以：   const model = await mobilenet.load();
 
+
+imageTensor:
+ {"dataId": {"id": 3056}, "dtype": "int32", "id": 2900, "isDisposedInternal": false,
+"kept": false, "rankType": "4", "scopeId": 1222, "shape": [1, 224, 224, 3], "size": 150528, "strides": [150528, 672, 3]}
 */
 
 import React, {useState, useEffect} from 'react';
@@ -22,77 +26,37 @@ import {
   TextInput,
   LogBox,
 } from 'react-native';
-import * as tf from '@tensorflow/tfjs';
 import {
-  fetch,
-  bundleResourceIO,
-  decodeJpeg,
-} from '@tensorflow/tfjs-react-native';
-import '@tensorflow/tfjs-react-native';
-import * as mobilenet from '@tensorflow-models/mobilenet';
-import * as jpeg from 'jpeg-js';
-import ImgToBase64 from 'react-native-image-base64';
-import Base64Arraybuffer from 'base64-arraybuffer';
-import {Base64Binary} from '../utils/base64Binary';
+  getModel,
+  convertBase64ToTensor,
+  startPrediction,
+} from './helpers/tensor-helper';
+import {cropPicture} from './helpers/image-helper';
+
 LogBox.ignoreLogs(['Require cycle:']);
 
 export default function TeachMachineTest(props) {
   const [url, setUrl] = useState(
-    'https://www.thespruce.com/thmb/2fz1zlPNq7cj7QkLAtKdqYrKvs0=/3704x2778/smart/filters:no_upscale()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg',
+    'https://www.appointmed.com/blog/wp-content/uploads/2017/11/rechnung-merkmale-appointmed.png',
   );
   const [displayText, setDisplayText] = useState('loading');
-  const [classDetector, setClassDetector] = useState();
-  const modelJson = require('../model/model.json');
-  const modelWeights = require('../model/weights.bin');
-  // 0: channel from JPEG-encoded image
-  // 1: gray scale
-  // 3: RGB image
-  const TENSORFLOW_CHANNEL = 3;
-  const BITMAP_DIMENSION = 224;
 
   const getPrediction = async url => {
     try {
       setDisplayText('Loading Tensor Flow...');
-      await tf.ready();
-      setDisplayText('Loading Teachable Machine...');
-
-      const model = await tf.loadLayersModel(
-        bundleResourceIO(modelJson, modelWeights),
-      );
-      setDisplayText('fetching image');
-      // const response = await fetch(url, {}, {isBinary: true});
-      // const imageData = await response.arrayBuffer;
-      // const base64 = Base64Arraybuffer.encode(imageData);
-      const base64 = await ImgToBase64.getBase64String(url);
-      console.log(base64);
-      // setDisplayText('creating image tensor');
-      // const imageTensor = convertBase64ToTensor(base64);
-      setDisplayText('done');
-      // setDisplayText('having prediction');
-      // console.log(imageTensor);
-      // const prediction = await model.predict(imageTensor);
-      // setDisplayText('done with prediction');
-      // console.log(prediction);
-      // setDisplayText(JSON.stringify([prediction]));
+      const model = await getModel();
+      setDisplayText('fetching url');
+      setDisplayText('resizing image');
+      const croppedData = await cropPicture(url);
+      setDisplayText('creating tensor');
+      const tensor = await convertBase64ToTensor(croppedData.base64);
+      console.log(tensor);
+      setDisplayText('predcting....');
+      const prediction = await startPrediction(model, tensor);
+      console.log(prediction);
+      setDisplayText(prediction);
     } catch (error) {
       console.log(error);
-    }
-  };
-
-  const convertBase64ToTensor = async base64 => {
-    try {
-      const uIntArray = Base64Binary.decode(base64);
-      // decode a JPEG-encoded image to a 3D Tensor of dtype
-      const decodedImage = decodeJpeg(uIntArray, 3);
-      // reshape Tensor into a 4D array
-      return decodedImage.reshape([
-        1,
-        BITMAP_DIMENSION,
-        BITMAP_DIMENSION,
-        TENSORFLOW_CHANNEL,
-      ]);
-    } catch (error) {
-      console.log('Could not convert base64 string to tesor', error);
     }
   };
 
